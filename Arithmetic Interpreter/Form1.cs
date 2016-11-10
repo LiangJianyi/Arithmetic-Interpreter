92a59a7715a094eec8eeca7b077003838e6a7490
梁建溢 <a.124p@live.com>
2016-11-10 12:28:28 +0800

    添加了语法检查

--- Arithmetic Interpreter/Form1.cs
+++ Arithmetic Interpreter/Form1.cs
@@ -13,11 +13,134 @@ namespace Arithmetic_Interpreter {
 			InitializeComponent( );
 			this.lblResult.Text = string.Empty;
 		}
-		
+
 		private void btnCalculating_Click( object sender , EventArgs e ) {
-			string[ ] tokens = this.Parse( this.txtInput.Text );
-			this.lblResult.Text = Evaluator.Calculating( tokens );
-			
+			if (this.txtInput.Text.Equals( string.Empty )) {
+				MessageBox.Show( "表达式不能为空，重新输入" , "警告" , MessageBoxButtons.OK , MessageBoxIcon.Warning );
+			}
+			else {
+				if (this.CheckLawlessness( this.txtInput.Text )) {
+					if (this.CheckSynax( this.txtInput.Text )) {
+						string[ ] tokens = this.Parse( this.txtInput.Text );
+						this.lblResult.Text = Evaluator.Calculating( tokens );
+					}
+					else {
+						MessageBox.Show( "运算符之间要包含数字或有效的表达式，重新输入" , "错误" , MessageBoxButtons.OK , MessageBoxIcon.Error );
+					}
+				}
+				else {
+					MessageBox.Show( "表达式含有非法字符，重新输入" , "错误" , MessageBoxButtons.OK , MessageBoxIcon.Error );
+				}
+			}
+		}
+
+		private bool CheckLawlessness( string express ) {
+			Predicate<int> containLegalCharacter = ( index ) => {
+				if (express.Substring( index , 1 ).Equals( "0" ) ||
+					express.Substring( index , 1 ).Equals( "1" ) ||
+					express.Substring( index , 1 ).Equals( "2" ) ||
+					express.Substring( index , 1 ).Equals( "3" ) ||
+					express.Substring( index , 1 ).Equals( "4" ) ||
+					express.Substring( index , 1 ).Equals( "5" ) ||
+					express.Substring( index , 1 ).Equals( "6" ) ||
+					express.Substring( index , 1 ).Equals( "7" ) ||
+					express.Substring( index , 1 ).Equals( "8" ) ||
+					express.Substring( index , 1 ).Equals( "9" )) {
+					return true;
+				}
+				else {
+					if (express.Substring( index , 1 ).Equals( "+" ) ||
+						express.Substring( index , 1 ).Equals( "-" ) ||
+						express.Substring( index , 1 ).Equals( "*" ) ||
+						express.Substring( index , 1 ).Equals( "/" ) ||
+						express.Substring( index , 1 ).Equals( "%" )) {
+						return true;
+					}
+					else {
+						return false;
+					}
+				}
+			};
+
+			const bool LAWLESSNESS_CHARACTER = false;
+			const bool LEGAL_CHARACTER = true;
+			bool result = true;
+			for (int index = 0 ; index < express.Length ; index++) {
+				if (containLegalCharacter( index )) {
+					result = LEGAL_CHARACTER;
+				}
+				else {
+					if (containLegalCharacter( index )) {
+						result = LEGAL_CHARACTER;
+					}
+					else {
+						result = LAWLESSNESS_CHARACTER;
+						break;
+					}
+				}
+			}
+
+			return result;
+		}
+
+		private bool CheckSynax( string express ) {
+			Predicate<int> isNumberCharacter = ( index ) => {
+				if (express.Substring( index , 1 ).Equals( "0" ) ||
+					express.Substring( index , 1 ).Equals( "1" ) ||
+					express.Substring( index , 1 ).Equals( "2" ) ||
+					express.Substring( index , 1 ).Equals( "3" ) ||
+					express.Substring( index , 1 ).Equals( "4" ) ||
+					express.Substring( index , 1 ).Equals( "5" ) ||
+					express.Substring( index , 1 ).Equals( "6" ) ||
+					express.Substring( index , 1 ).Equals( "7" ) ||
+					express.Substring( index , 1 ).Equals( "8" ) ||
+					express.Substring( index , 1 ).Equals( "9" )) {
+					return true;
+				}
+				else {
+					return false;
+				}
+			};
+			Predicate<int> isOperatorCharacter = ( index ) => {
+				if (express.Substring( index , 1 ).Equals( "+" ) ||
+					express.Substring( index , 1 ).Equals( "-" ) ||
+					express.Substring( index , 1 ).Equals( "*" ) ||
+					express.Substring( index , 1 ).Equals( "/" ) ||
+					express.Substring( index , 1 ).Equals( "%" )) {
+					return true;
+				}
+				else {
+					return false;
+				}
+			};
+
+			bool lastCharacterIsNumber = true;
+			if (isNumberCharacter( 0 ) && isNumberCharacter( express.Length - 1 )) {
+				for (int index = 1 ; index < express.Length - 1 ; index++) {
+					if (isNumberCharacter( index )) {
+						if (lastCharacterIsNumber) {
+							continue;
+						}
+						else {
+							lastCharacterIsNumber = !lastCharacterIsNumber;
+						}
+					}
+					else if (isOperatorCharacter( index )) {
+						if (lastCharacterIsNumber) {
+							lastCharacterIsNumber = !lastCharacterIsNumber;
+							continue;
+						}
+						else {
+							return false;
+						}
+					}
+				}
+			}
+			else {
+				return false;
+			}
+
+			return true;
 		}
 
 		private string[ ] Parse( string express ) {
