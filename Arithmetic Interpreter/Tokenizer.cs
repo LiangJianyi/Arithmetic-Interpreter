93ec0ce85d65174b5b13035465058916f645c79d
建溢 梁 <a.124p@live.com>
2017-03-18 12:08:27 +0800

    开发进度临时中断

--- Arithmetic Interpreter/Tokenizer.cs
+++ Arithmetic Interpreter/Tokenizer.cs
@@ -9,19 +9,20 @@ namespace Arithmetic_Interpreter {
 			int index = 0;
 			Predicate<char> isNumber = ch => ch == '0' || ch == '1' || ch == '2' || ch == '3' || ch == '4' || ch == '5' || ch == '6' || ch == '7' || ch == '8' || ch == '9';
 			Predicate<char> isOperator = ch => ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '%';
-			Func<bool> isSqrt = ( ) => {
+			Predicate<int> isSqrt = ( i ) => {
 				string word = String.Empty;
 				Action next = ( ) => {
-					word += express[ index ];
-					index++;
+					word += express[ i ];
+					i++;
 				};
-				if (express[ index ] == 's' || express[ index ] == 'S') {
+				if (express[ i ] == 's' || express[ i ] == 'S') {
 					next( );
-					if (express[ index ] == 'q' || express[ index ] == 'Q') {
+					if (express[ i ] == 'q' || express[ i ] == 'Q') {
 						next( );
-						if (express[ index ] == 'r' || express[ index ] == 'R') {
+						if (express[ i ] == 'r' || express[ i ] == 'R') {
 							next( );
-							if (express[ index ] == 't' || express[ index ] == 'T') {
+							if (express[ i ] == 't' || express[ i ] == 'T') {
+								index = i;
 								return true;
 							}
 						}
@@ -29,36 +30,108 @@ namespace Arithmetic_Interpreter {
 				}
 				return false;
 			};
-			Func<bool> isPi = ( ) => {
+			Predicate<int> isPi = ( i ) => {
 				string word = String.Empty;
 				Action next = ( ) => {
-					word += express[ index ];
-					index++;
+					word += express[ i ];
+					i++;
 				};
-				if (express[ index ] == 'p' || express[ index ] == 'P') {
+				if (express[ i ] == 'p' || express[ i ] == 'P') {
 					next( );
-					if (express[ index ] == 'i' || express[ index ] == 'I') {
+					if (express[ i ] == 'i' || express[ i ] == 'I') {
+						index = i;
 						return true;
 					}
 				}
 				return false;
 			};
-			Action leftRacketOfNext = ( ) => {
-
+			Func<bool> leftRacketOfNext = ( ) => {
+				if (index < express.Length - 1) {
+					int next = index - 1;
+					if (isNumber( express[ next ] ) || express[ next ] == '-' || isSqrt( express[ next ] ) || express[ next ] == '(') {
+						return true;
+					}
+					else {
+						throw new InvalidOperationException( "左圆括号右边只能是数字、负号、sqrt、左圆括号" );
+					}
+				}
+				else {
+					throw new InvalidOperationException( "左圆括号没有匹配的右圆括号。" );
+				}
 			};
+			Func<bool> numberOfNext = ( ) => {
+				if (index < express.Length - 1) {
+					int next = index - 1;
+					if (express[ next ] == ')' || express[ next ] == '^' || express[ next ] == '.' || isOperator( express[ next ] ) || express[ next ] == '!') {
+						return true;
+					}
+					else {
+						throw new InvalidOperationException( "数字右边只能是\")\", \"^\", \"!\", 小数点, 算术运算符" );
+					}
+				}
+				else {
+					return true;	// 数字可以作为最后一个字符
+				}
+			};
+			Func<bool> plusOfNext = () =>{
+				if (index < express.Length - 1) {
+					int next = index - 1;
+					if (isNumber( express[ next ] ) || express[ next ] == '+' || express[ next ] == '-' || isSqrt( express[ next ] ) || express[ next ] == '(') {
+						return true;
+					}
+					else {
+						throw new InvalidOperationException( "算术运算符的右边只能是加号、减号、数字、sqrt、左圆括号" );
+					}
+				}
+				else {
+					throw new InvalidOperationException( "算术运算符不能用作表达式的结尾。" );
+				}
+			};
+			Func<bool> minusOfNext = ( ) => {
+				if (index < express.Length - 1) {
+					int next = index - 1;
+					if (isNumber( express[ next ] ) || express[ next ] == '+' || express[ next ] == '-' || isSqrt( express[ next ] ) || express[ next ] == '(') {
+						return true;
+					}
+					else {
+						throw new InvalidOperationException( "算术运算符的右边只能是加号、减号、数字、sqrt、左圆括号" );
+					}
+				}
+				else {
+					throw new InvalidOperationException( "算术运算符不能用作表达式的结尾。" );
+				}
+			};
+			Func<bool> decimalpointOfNext = ( ) => {
+				if (index < express.Length - 1) {
+					int next = index - 1;
+					if (isNumber( express[ next ] )) {
+						return true;
+					}
+					else {
+						throw new InvalidOperationException( "小数点的右边只能是数字" );
+					}
+				}
+				else {
+					throw new InvalidOperationException( "小数点不能用作表达式的结尾。" );
+				}
+			};
+
 
 			for (; index < express.Count( ) ; index++) {
 				if (index < 1) {
 					if (express[ index ] == '(') {
 
 					}
-					else if (isSqrt( )) {
+					else if (isSqrt( index )) {
 
 					}
-					else if (isPi( )) {
+					else if (isPi( index )) {
 
 					}
-					else if (isOperator( express[ index ] )) {
+					else if (express[index]=='+') {
+
+					}
+					else if (express[index]=='-') {
 
 					}
 					else if (isNumber( express[ index ] )) {
@@ -72,10 +145,10 @@ namespace Arithmetic_Interpreter {
 					if (express[ index ] == '(') {
 
 					}
-					else if (isSqrt( )) {
+					else if (isSqrt( index )) {
 
 					}
-					else if (isPi( )) {
+					else if (isPi( index )) {
 
 					}
 					else if (isOperator( express[ index ] )) {
