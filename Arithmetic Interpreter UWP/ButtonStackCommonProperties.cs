7955ca08b2ede8ea06b576c0cd0255a030ef3b63
梁建溢 <liangjianyi.com@outlook.com>
2018-06-22 11:26:26 -0400

    Arithmetic Interpreter UWP--Changesets 01

---
+++ Arithmetic Interpreter UWP/ButtonStackCommonProperties.cs
@@ -0,0 +1,75 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media;
+
+namespace Arithmetic_Interpreter_UWP {
+	class ButtonStackCommonProperties : INotifyPropertyChanged {
+		private double _width;
+		private double _height;
+		private SolidColorBrush _foreground;
+		private double _fontsize;
+		private Thickness _margin;
+
+		public double Width {
+			get
+			{
+				return this._width;
+			}
+			set
+			{
+				this._width = value;
+				this.OnPropertyChanged();
+			}
+		}
+		public double Height {
+			get
+			{
+				return this._height;
+			}
+			set
+			{
+				this._height = value;
+				OnPropertyChanged();
+			}
+		}
+		public SolidColorBrush Foreground { 
+			get
+			{
+				return this._foreground;
+			}
+			set
+			{
+				this._foreground = value;
+				OnPropertyChanged();
+			}
+		}
+		public double FontSize {
+			get
+			{
+				return this._fontsize;
+			}
+			set
+			{
+				this._fontsize = value;
+				OnPropertyChanged();
+			}
+		}
+		public Thickness Margin {
+			get
+			{
+				return this._margin;
+			}
+			set
+			{
+				this._margin = value;
+				OnPropertyChanged();
+			}
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+		public void OnPropertyChanged([CallerMemberName]string propertyName = "") {
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+}
---
+++ Arithmetic Interpreter UWP/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace Arithmetic_Interpreter_UWP {
+	public sealed partial class MainPage : Page {
+		public MainPage() {
+			this.InitializeComponent();
+			if (Resources["buttonStackCommonProperties"] is ButtonStackCommonProperties buttonStackCommonProperties) {
+				buttonStackCommonProperties.Width = 100;
+				buttonStackCommonProperties.Height = 50;
+				buttonStackCommonProperties.Foreground = new SolidColorBrush(Windows.UI.Colors.AliceBlue);
+				buttonStackCommonProperties.FontSize = 18;
+				buttonStackCommonProperties.Margin = new Thickness(0, 20, 0, 0);
+			}
+		}
+
+		private void SetFocusButton_Click(object sender, RoutedEventArgs e) {
+			CodeEditor.Focus(FocusState.Programmatic);
+		}
+
+		private void CodeEditor_PointerPressed(object sender, PointerRoutedEventArgs e) {
+			System.Diagnostics.Debug.WriteLine("fired pointer presse.");
+		}
+
+		private void CodeEditor_PointerEntered(object sender, PointerRoutedEventArgs e) {
+			System.Diagnostics.Debug.WriteLine("fired pointer enter.");
+		}
+
+		private void CodeEditor_PointerReleased(object sender, PointerRoutedEventArgs e) {
+			System.Diagnostics.Debug.WriteLine("fired pointer release.");
+		}
+
+		private void Root_Loading(FrameworkElement sender, object args) {
+			CodeEditor.AddHandler(UIElement.PointerPressedEvent, new PointerEventHandler(CodeEditor_PointerPressed), true);
+		}
+	}
+}
