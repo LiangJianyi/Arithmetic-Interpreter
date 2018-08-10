6d5e506d0edbc3ed0a0cddfbfb22d7d0efe86345
梁建溢 <liangjianyi.com@outlook.com>
2018-08-10 09:57:52 -0400

    Add SchemeProcedures and cons

---
+++ SchemeProcedures/cons.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Arithmetic_Interpreter_UWP {
+	public class Cons<T> : LinkedList<T> {
+		private T _car;
+		private T _cdr;
+
+		public Cons(T car, T cdr) {
+			this._car = car;
+			this._cdr = cdr;
+		}
+
+		public T Car {
+			get => this._car;
+			set
+			{
+				this._car = value;
+			}
+		}
+		public T Cdr {
+			get => this._cdr;
+			set
+			{
+				this._cdr = value;
+			}
+		}
+
+		public override bool Equals(object obj) {
+			if (obj is Cons<T> target) {
+				return EqualityComparer<T>.Default.Equals(this.Car, target.Car) && EqualityComparer<T>.Default.Equals(this.Cdr, target.Cdr);
+			}
+			else {
+				return false;
+			}
+		}
+
+		public override int GetHashCode() {
+			return this._car.GetHashCode() ^ this._cdr.GetHashCode();
+		}
+	}
+}
