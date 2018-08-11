69fdd737c29333ded5d14683fbc24654aa54255e
梁建溢 <liangjianyi.com@outlook.com>
2018-08-11 01:18:24 -0400

    解除了对 LinkedList 的继承，添加了 Cons<Tcar,Tcdr> 的重载类型，相等判断的运算符解除对 Equals 方法的依赖

--- SchemeProcedures/cons.cs
+++ SchemeProcedures/cons.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 
 namespace Arithmetic_Interpreter_UWP {
-	public class Cons<T> : LinkedList<T> {
+	public class Cons<T> {
 		private T _car;
 		private T _cdr;
 
@@ -40,7 +40,59 @@ namespace Arithmetic_Interpreter_UWP {
 		public override int GetHashCode() {
 			return this._car.GetHashCode() ^ this._cdr.GetHashCode();
 		}
-		public static bool operator ==(Cons<T> consLeft, Cons<T> consRight) => consLeft.Equals(consRight);
-		public static bool operator !=(Cons<T> consLeft, Cons<T> consRight) => !(consLeft.Equals(consRight));
+		public override string ToString() {
+			return $"cons({this._car}, {this._cdr})";
+		}
+		public static bool operator ==(Cons<T> consLeft, Cons<T> consRight) => 
+			EqualityComparer<T>.Default.Equals(consLeft.Car, consRight.Car) && EqualityComparer<T>.Default.Equals(consLeft.Cdr, consRight.Cdr);
+		public static bool operator !=(Cons<T> consLeft, Cons<T> consRight) => 
+			!(EqualityComparer<T>.Default.Equals(consLeft.Car, consRight.Car) && EqualityComparer<T>.Default.Equals(consLeft.Cdr, consRight.Cdr));
+	}
+
+	public class Cons<Tcar,Tcdr> {
+		private Tcar _car;
+		private Tcdr _cdr;
+
+		public Cons(Tcar car, Tcdr cdr) {
+			this._car = car;
+			this._cdr = cdr;
+		}
+
+		public Tcar Car {
+			get => this._car;
+			set
+			{
+				this._car = value;
+			}
+		}
+		public Tcdr Cdr {
+			get => this._cdr;
+			set
+			{
+				this._cdr = value;
+			}
+		}
+
+		public static Tcar GetCar(Cons<Tcar,Tcdr> cons) => cons._car;
+		public static Tcdr GetCdr(Cons<Tcdr,Tcdr> cons) => cons._cdr;
+
+		public override bool Equals(object obj) {
+			if (obj is Cons<Tcar,Tcdr> target) {
+				return EqualityComparer<Tcar>.Default.Equals(this.Car, target.Car) && EqualityComparer<Tcdr>.Default.Equals(this.Cdr, target.Cdr);
+			}
+			else {
+				return false;
+			}
+		}
+		public override int GetHashCode() {
+			return this._car.GetHashCode() ^ this._cdr.GetHashCode();
+		}
+		public override string ToString() {
+			return $"cons({this._car}, {this._cdr})";
+		}
+		public static bool operator ==(Cons<Tcar,Tcdr> consLeft, Cons<Tcar,Tcdr> consRight) => 
+			EqualityComparer<Tcar>.Default.Equals(consLeft.Car, consRight.Car) && EqualityComparer<Tcdr>.Default.Equals(consLeft.Cdr, consRight.Cdr);
+		public static bool operator !=(Cons<Tcar, Tcdr> consLeft, Cons<Tcar, Tcdr> consRight) =>
+			!(EqualityComparer<Tcar>.Default.Equals(consLeft.Car, consRight.Car) && EqualityComparer<Tcdr>.Default.Equals(consLeft.Cdr, consRight.Cdr));
 	}
 }
