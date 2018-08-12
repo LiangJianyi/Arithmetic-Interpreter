f9e68b52a91f8e4908903cec1dff6193c5401cbf
梁建溢 <liangjianyi.com@outlook.com>
2018-08-12 09:51:47 -0400

    把 .NET Standard 类库的版本降到了 1.4，目前正在测试 CodeEditor 的字符编码

--- Arithmetic Interpreter UWP/MainPage.xaml.cs
+++ Arithmetic Interpreter UWP/MainPage.xaml.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
 using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Documents;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
@@ -19,9 +19,25 @@ namespace Arithmetic_Interpreter_UWP {
 
 	public sealed partial class MainPage : Page {
 		private Window _window = Window.Current;
+		/// <summary>
+		/// 指示 ConsoleWindowSpring() 的行为
+		/// </summary>
+		private bool _consoleState = false;
 
 		public MainPage() {
 			this.InitializeComponent();
+
+			/*
+			 * 为了让用户单击面板类控件实现关闭 Console，需要捕获所有从非 Button 控件路由上来的 PointerPressed 事件，
+			 * 这样用户单击非运行按钮和 Console 以外的控件时实现关闭 Console。但为了避免运行按钮的 PointerPressed
+			 * 处理程序结束后也路由到其它能够关闭 Console 的事件处理程序，因此需要在负责打开 Console 的事件处理程序中
+			 * 执行 e.Handler=true 并只对 RunAppBarButton 的 AddHandler 的 handledEventsToo 参数传递 true，
+			 * 这样当打开 Console 时所触发的事件处理程序会立即标记完成并停止向上路由，从而避免了 Console 一打开就立即关闭的现象。
+			 */
+			RunAppBarButton.AddHandler(UIElement.PointerPressedEvent, new PointerEventHandler(AppBarRunButton_PointerPressed), true);
+			CommandBar.AddHandler(UIElement.PointerPressedEvent, new PointerEventHandler(this.CloseConsole_PointerPressed), false);
+			Root.AddHandler(UIElement.PointerPressedEvent, new PointerEventHandler(this.CloseConsole_PointerPressed), false);
+
 			if (Resources["buttonStackCommonProperties"] is ButtonStackCommonProperties buttonStackCommonProperties) {
 				buttonStackCommonProperties.Width = 100;
 				buttonStackCommonProperties.Height = 50;
@@ -29,7 +45,15 @@ namespace Arithmetic_Interpreter_UWP {
 				buttonStackCommonProperties.FontSize = 18;
 				buttonStackCommonProperties.Margin = new Thickness(0, 20, 0, 0);
 			}
+
 			SetTitleBarTheme();
+		}
+
+		private void CloseConsole_PointerPressed(object sender, RoutedEventArgs e) {
+			if (this._consoleState) {
+				this._consoleState = !this._consoleState;
+			}
+			this.ConsoleWindowSpring();
 		}
 
 		private static void SetTitleBarTheme() {
@@ -58,17 +82,17 @@ namespace Arithmetic_Interpreter_UWP {
 				point: out Point point
 			);
 			Debug.WriteLine($"point: {point.ToString()}");
-			MenuFlyout menuFlyout = CreateMenuFlyout(new List<string>() { "Google","Apple","Microsoft" });
+			MenuFlyout menuFlyout = CreateMenuFlyout(new List<string>() { "Google", "Apple", "Microsoft" });
 			switch (e.Key) {
 				case Windows.System.VirtualKey.Q:
 					this.ShowIntellisense(point.X + 10, point.Y + 10, (x, y) => {
 						menuFlyout.ShowAt(richEditBox, new Point(x, y));
-					}); 
+					});
 					break;
 				case Windows.System.VirtualKey.E:
 					this.ShowIntellisense(point.X + 10, point.Y + 10, (x, y) => {
 						menuFlyout.ShowAt(richEditBox, new Point(x, y));
-					}); 
+					});
 					break;
 				default:
 					break;
@@ -104,7 +128,7 @@ namespace Arithmetic_Interpreter_UWP {
 			}
 			catch (ArgumentException argexp) {
 				// Coordinates out of range
-				System.Diagnostics.Debug.WriteLine(argexp.Message);
+				Debug.WriteLine(argexp.Message);
 			}
 		}
 
@@ -121,6 +145,52 @@ namespace Arithmetic_Interpreter_UWP {
 		private static bool IsShiftKeyPressed() {
 			var shiftState = Windows.UI.Core.CoreWindow.GetForCurrentThread().GetKeyState(Windows.System.VirtualKey.Shift);
 			return (shiftState & Windows.UI.Core.CoreVirtualKeyStates.Down) == Windows.UI.Core.CoreVirtualKeyStates.Down;
+		}
+
+		private void AppBarRunButton_PointerPressed(object sender, PointerRoutedEventArgs e) {
+			this._consoleState = true;
+			this.ConsoleWindowSpring();
+			e.Handled = true;
+
+			CodeEditor.Document.GetText(Windows.UI.Text.TextGetOptions.FormatRtf, out string result);
+			LinkedList<string> tokens = Tokenizer.GetTokens(result);
+			LinkedList<Paragraph> paragraphs = new LinkedList<Paragraph>();
+			foreach (var token in tokens) {
+				Encoding utf8 = Encoding.UTF8;
+				byte[] bytes = utf8.GetBytes(token);
+				Paragraph paragraph = new Paragraph();
+				paragraph.Inlines.Add(new Run() { Text = utf8.GetString(bytes, 0, bytes.Length) });
+				Console.Blocks.Add(paragraph);
+			}
+		}
+
+		/// <summary>
+		/// 负责 Console 的开关，当字段 _consoleState 为 true 时打开 Console，否则关闭 Console
+		/// </summary>
+		private void ConsoleWindowSpring() {
+			if (this._consoleState) {
+				if (Root.RowDefinitions[2].Height.Value <= 0) {
+					Root.RowDefinitions[2].Height = new GridLength(this._window.Bounds.Height * 0.2);
+					Console.Width = this._window.Bounds.Width;
+					Console.Height = this._window.Bounds.Height * 0.2;
+				}
+			}
+			else {
+				Root.RowDefinitions[2].Height = new GridLength(0);
+				Console.Width = 0;
+				Console.Height = 0;
+			}
+		}
+	}
+
+	public static class Tokenizer {
+		public static LinkedList<string> GetTokens(string text) {
+			string[] tokens = text.Split(new char[] { ' ' });
+			LinkedList<string> result = new LinkedList<string>();
+			foreach (var token in tokens) {
+				result.AddLast(token);
+			}
+			return result;
 		}
 	}
 }
