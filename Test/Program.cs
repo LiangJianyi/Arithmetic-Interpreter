7832a5df68c743ac27f3b9cf23ee39406450b3bc
梁建溢 <liangjianyi.com@outlook.com>
2018-09-07 07:34:13 -0400

    实验一种依赖于 System.Collection.Generic.LinkedList<T> 的 cons 容器 - 4

--- Test/Program.cs
+++ Test/Program.cs
@@ -44,43 +44,48 @@ namespace Test {
 			Cons2 c2 = new Cons2("l", c1);
 			Cons2 c3 = new Cons2("+", c2);
 			Cons2 c4 = new Cons2("r");
-			Cons2 c5 = new Cons2("1", c4);
+			Cons2 c5 = new Cons2("l", c4);
 			Cons2 c6 = new Cons2("add", c5);
 			Cons2 c7 = new Cons2(c3);
 			Cons2 c8 = new Cons2(c6, c7);
 			Cons2 c9 = new Cons2("define", c8);
-			List<Cons2> ccc = new List<Cons2>() { c1, c2, c3, c4, c5, c6, c7, c8, c9 };
-			//foreach (var item in ccc) {
-			//	Print(item.Car().Previous);
-			//	Print(item.Car().Next);
-			//	Print(item.Cdr().Previous);
-			//	Print(item.Cdr().Next);
-			//	Print(item.CarValue());
-			//	Print(item.CdrValue());
-			//}
-			//Print2(c9);
-			LinkedList<Cons2> fuck = new LinkedList<Cons2>();
-			fuck.AddFirst(c9);
-			Console.WriteLine(fuck.First.Value.CarValue());
-			Console.WriteLine(fuck.Last.Value.CarValue());
-			Console.WriteLine(fuck.First.Previous);
-			Console.WriteLine(fuck.First.Next);
-			Console.WriteLine(fuck.Last.Previous);
-			Console.WriteLine(fuck.Last.Next);
+			Dictionary<string, Cons2> ccc = new Dictionary<string, Cons2>() {
+				{ "c1", c1 },
+				{ "c2", c2 },
+				{ "c3", c3 },
+				{ "c4", c4 },
+				{ "c5", c5 },
+				{ "c6", c6 },
+				{ "c7", c7 },
+				{ "c8", c8 },
+				{ "c9", c9 }
+			};
+			foreach (var item in ccc) {
+				Console.WriteLine(item.Key);
+				Console.WriteLine($"cons.Car().Previous: {item.Value.Car().Previous}");
+				Console.WriteLine($"cons.Car().Next: {item.Value.Car().Next}");
+				Console.WriteLine($"cons.Cdr().Previous: {item.Value.Cdr().Previous}");
+				Console.WriteLine($"cons.Cdr().Next: {item.Value.Cdr().Next}");
+				Console.WriteLine($"cons.CarValue(): {item.Value.CarValue()}");
+				Console.WriteLine($"cons.CdrValue(): {item.Value.CdrValue()}");
+			}
 		}
 
 		private static void Print2(Cons2 item) {
-			Print(item.Car().Previous);
-			Print(item.Car().Next);
-			Print(item.Cdr().Previous);
-			Print(item.Cdr().Next);
-			Print(item.CarValue());
-			Print(item.CdrValue());
+			Console.WriteLine(item.Car().Previous);
+			Console.WriteLine(item.Car().Next);
+			Console.WriteLine(item.Cdr().Previous);
+			Console.WriteLine(item.Cdr().Next);
+			Console.WriteLine(item.CarValue());
+			Console.WriteLine(item.CdrValue());
 		}
 
 		private static void Print(object node) {
 			if (node == null) {
 				Console.WriteLine("null");
+			}
+			else if (node is ValueType vt) {
+				Console.WriteLine(vt);
 			}
 			else if (node is LinkedListNode<int> liknode) {
 				if (node == null) {
